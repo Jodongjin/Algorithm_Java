8c224b4e7f1cb8754ebc3f3440db90686c004081
Jodongjin <ehdwlsdlwkd@naver.com>
2023-04-19 18:14:52 +0900

    프로그래머스_코딩테스트 입문

---
+++ src/main/java/com/example/algorithm/Test.java
@@ -0,0 +1,50 @@
+package com.example.algorithm;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+        int answer = solution(new int[]{3, 10, 28}, 200);
+        System.out.println("answer = " + answer);
+    }
+
+    public static int solution(int[] array, int n) {
+        int answer = 0;
+        int[] arr = new int[array.length];
+
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = Math.abs(array[i] - n);
+        }
+
+        int min = Arrays.stream(arr).min().getAsInt();
+        int minIdx = 0;
+//        int minIdx = Arrays.asList(arr).indexOf(min);
+
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] == min)
+                minIdx = i;
+        }
+
+        answer = array[minIdx];
+
+        return answer;
+    }
+
+    public static int solution2(int[] array, int n) {
+        int answer = 0;
+        int min = Math.abs(array[0] - n);
+        int minIdx = 0;
+
+        Arrays.sort(array);
+        for(int i = 1; i < array.length; i++) {
+            if(Math.abs(array[i] - n) < min) {
+                min = Math.abs(array[i] - n);
+                minIdx = i;
+            }
+        }
+        answer = array[minIdx];
+
+        return answer;
+    }
+}
