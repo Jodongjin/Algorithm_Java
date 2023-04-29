51fe54d53637617be9167b28225798e6a98f1b5c
Jodongjin <ehdwlsdlwkd@naver.com>
2023-04-29 21:00:31 +0900

    23/04/29 프로그래머스 고득점 Kit

--- src/main/java/com/example/algorithm/Test.java
+++ src/main/java/com/example/algorithm/Test.java
@@ -6,28 +6,17 @@ import java.util.stream.Collectors;
 
 public class Test {
     public static void main(String[] args) {
-        int answer = solution(new int[][]{{1, 4}, {9, 2}, {3, 8}, {11, 6}});
+        int answer = solution(new int[]{3, 1, 2, 3});
         System.out.println("answer = " + answer);
     }
 
     /**
-     * a1 a2 / a3 a4    a1 a3 / a2 a4    a1 a4 / a2 a3    세 가지 경우
-     * 각 경우마다 기울기 구해서 비교, 일치하면 answer = 1
+     * pick num은 nums.length / 2
+     * nums 중복 제거 count >= pick num => return pick num
+     * nums 중복 제거 count < pick num => return 중복 제거 count
      */
-    public static int solution(int[][] dots) {
+    public static int solution(int[] nums) {
         int answer = 0;
-
-        // case 1
-        if((double) (dots[1][1] - dots[0][1]) / (dots[1][0] - dots[0][0]) ==
-                (double) (dots[3][1] - dots[2][1]) / (dots[3][0] - dots[2][0]))
-            answer = 1;
-        else if((double) (dots[2][1] - dots[0][1]) / (dots[2][0] - dots[0][0]) ==
-                (double) (dots[3][1] - dots[1][1]) / (dots[3][0] - dots[1][0]))
-            answer = 1;
-        else if((double) (dots[3][1] - dots[0][1]) / (dots[3][0] - dots[0][0]) ==
-                (double) (dots[2][1] - dots[1][1]) / (dots[2][0] - dots[1][0]))
-            answer = 1;
-
         return answer;
     }
 }
--- src/main/java/com/example/algorithm/programmers/옹알이_1.java
+++ src/main/java/com/example/algorithm/programmers/옹알이_1.java
@@ -6,22 +6,37 @@ public class 옹알이_1 {
         System.out.println("answer = " + answer);
     }
 
+    // 테스트 3개 정도 실패..
     public static int solution(String[] babbling) {
         int answer = 0;
-        String[] words = new String[]{"aya", "ye", "woo", "ma"};
+        String[] words = new String[]{"aya", "woo", "ye", "ma"};
 
         for(String str : babbling) {
             String temp = str;
             for(String word : words) {
-                System.out.println("str = " + str);
-                System.out.println("word = " + word);
                 temp = temp.replace(word, "");
-                System.out.println("temp = " + temp);
             }
             if(temp.length() == 0) {
                 answer++;
             }
 
+        }
+
+        return answer;
+    }
+
+    public static int solution2(String[] babbling) {
+        int answer = 0;
+
+        for(int i =0; i < babbling.length; i++) {
+            babbling[i] = babbling[i].replace("aya", "1");
+            babbling[i] = babbling[i].replace("woo", "1");
+            babbling[i] = babbling[i].replace("ye", "1");
+            babbling[i] = babbling[i].replace("ma", "1");
+            babbling[i] = babbling[i].replace("1", "");
+            if(babbling[i].isEmpty()) {
+                answer = answer + 1;
+            }
         }
 
         return answer;
