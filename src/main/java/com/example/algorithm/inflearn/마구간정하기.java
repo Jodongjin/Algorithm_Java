5b1e2d79b4a8a9f5742470ae765226f0da18f1c4
Jodongjin <ehdwlsdlwkd@naver.com>
2023-06-05 22:10:34 +0900

    23/06/05 inflearn

--- src/main/java/com/example/algorithm/inflearn/마구간정하기.java
+++ src/main/java/com/example/algorithm/inflearn/마구간정하기.java
@@ -1,5 +1,6 @@
 package com.example.algorithm.inflearn;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class 마구간정하기 {
@@ -12,12 +13,40 @@ public class 마구간정하기 {
         for(int i = 0; i < n; i++)
             arr[i] = kb.nextInt();
 
-        System.out.println(T.my_solution(n, c, arr));
+        System.out.println(T.solution(n, c, arr));
     }
 
-    public int my_solution(int n, int c, int[] arr) {
+    /**
+     * 결정 알고리즘에서 lt, rt의 대상은 정답으로 정하면 됨
+     * lt, rt를 두 말의 최대 거리 (정답)으로 두면 됨
+     */
+    public int solution(int n, int c, int[] arr) {
         int answer = 0;
+        Arrays.sort(arr);
+        int lt = 1;
+        int rt = arr[n - 1]; // arr[n - 1] - arr[0]으로 하면 범위를 더 좁힐 수 있음
+        while (lt <= rt) {
+            int mid = (lt + rt) / 2;
+            if(count(arr, mid) >= c) {
+                answer = mid;
+                lt = mid + 1;
+            }
+            else rt = mid - 1;
+        }
 
         return answer;
+    }
+
+    public int count(int[] arr, int dist) {
+        int cnt = 1; // 말의 수, 처음에 0 idx에 배치 -> 1
+        int ep = arr[0]; // 마지막 배치 위치
+        for(int i = 1; i < arr.length; i++) {
+            if(arr[i] - ep >= dist) {
+                cnt++;
+                ep = arr[i];
+            }
+        }
+
+        return cnt;
     }
 }
