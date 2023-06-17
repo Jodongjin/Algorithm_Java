d16414d7b3e4ea8e8fa0d1ecef3ff2484a6fe1f2
Jodongjin <ehdwlsdlwkd@naver.com>
2023-06-18 03:47:10 +0900

    23/06/18 programmers

--- src/main/java/com/example/algorithm/programmers/행렬의곱셈.java
+++ src/main/java/com/example/algorithm/programmers/행렬의곱셈.java
@@ -8,13 +8,24 @@ public class 행렬의곱셈 {
         int[][] arr1 = {{1, 4}, {3, 2}, {4, 1}};
         int[][] arr2 = {{3, 3}, {3, 3}};
 
-        for(int[] arr : T.my_solution(arr1, arr2))
+        for(int[] arr : T.solution(arr1, arr2))
             System.out.println(Arrays.toString(arr));
     }
 
-    public int[][] my_solution(int[][] arr1, int[][] arr2) {
-        int[][] answer = new int[arr1.length][arr1.length];
+    /**
+     * 행렬의 곱 개념을 몰랐다 ,, ㅜㅜ
+     */
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr2[0].length];
 
+        for(int i = 0; i < arr1.length; i++)
+            for(int j = 0; j < arr2[0].length; j++) {
+                int temp = 0;
+                for (int k = 0; k < arr2.length; k++)
+                    temp += arr1[i][k] * arr2[k][j];
+
+                answer[i][j] = temp;
+            }
         return answer;
     }
 }
